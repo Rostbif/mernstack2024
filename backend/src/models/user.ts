3cb240e774dddfc4ccbe61e19bec04be990c6842
Ofir Adany <ofiradany@gmail.com>
2024-06-03 11:37:26 +0300

    tuna

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -9,6 +9,7 @@ export type UserType = {
     lastname: string;
 }
 
+
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
