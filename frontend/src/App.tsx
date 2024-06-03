b81b34c89ed93e626554892daf9ef577570ebcb1
Ofir Adany <ofiradany@gmail.com>
2024-06-03 15:05:03 +0300

    added register form + learned how to debug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import {
   Navigate
 } from "react-router-dom";
 import Layout from './layouts/Layout';
+import Register from './pages/Register';
 // import './App.css'
 
 function App() {
@@ -25,6 +26,11 @@ function App() {
         {
           <Layout>
             <p> Search Page</p>
+          </Layout>
+        }/>
+        <Route path="/register" element={
+          <Layout>
+            <Register/>
           </Layout>
         }/>
         <Route path="*" element={<Navigate to="/"/>}/>
