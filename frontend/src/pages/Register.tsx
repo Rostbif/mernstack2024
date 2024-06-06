c63b2b24f3b996efac96ec553079bf0e26a3fc9a
Ofir Adany <ofiradany@gmail.com>
2024-06-06 11:35:26 +0300

    3:29 hour

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -114,12 +114,14 @@ const Register = () => {
           </span>
         )}
       </label>
-      <button
-        type="submit"
-        className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-x1"
-      >
-        Create Account
-      </button>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-x1"
+        >
+          Create Account
+        </button>
+      </span>
     </form>
   );
 };
