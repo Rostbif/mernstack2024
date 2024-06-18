4951cee62e521f527d4a0c131051f7c9a5048e78
Ofir Adany <ofiradany@gmail.com>
2024-06-18 19:31:29 +0300

    many things!! including the detail page

---
+++ frontend/src/pages/Detail.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "./../api-client";
+import { AiFillStar } from "react-icons/ai";
+
+const Detail = () => {
+  const { hotelId } = useParams();
+
+  const { data: hotel } = useQuery(
+    "fetchHotelById",
+    () => apiClient.fetchHotelById(hotelId as string),
+    { enabled: !!hotelId }
+  );
+
+  if (!hotel) {
+    return <></>;
+  }
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <span className="flex">
+          {Array.from({ length: hotel.starRating }).map(() => (
+            <AiFillStar className="fill-yellow-400" />
+          ))}
+        </span>
+        <h1 className="text-3xl font-bold">{hotel.name}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
